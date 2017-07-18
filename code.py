da2c4658de4888fda763a1321db318b06d75138d
SarthakNijhawan <sarthakn69@gmail.com>
2017-07-18 07:17:33 +0530

    Crude form in progress

--- code.py
+++ code.py
@@ -6,7 +6,24 @@ import numpy as np
 import sys
 import matplotlib.pyplot as plt
 
+def processed_img(img):
+	"Takes a greyscale image as an input."
+	# Locally Adaptive Binarisation 
+	adap_thresh = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 3, 2) #TODO
+	
+	# Noise Reduction
+	median = cv2.meadianBlur(adap_thresh, 3)
+	
+	# Region Labeling
+	
+		
+	
+	# Morphological Thinning
+	raise NotImplementedError
+
+def algo(img):
+	raise NotImplementedError
 
 if __name__=="__main__":
-	input_img = cv2.imread(sys.argv[1], 1)
+	input_img = cv2.imread(sys.argv[1], 0)
 	pass
